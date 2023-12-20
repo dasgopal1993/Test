5c6c67d89f381861f16ca0ada113b7b3961a32c9
dasgopal0999@gmail.com <dasgopal0999@gmail.com>
2023-12-20 14:46:16 +0530

    sdded

---
+++ src/main/java/com/example/Test/FactorialCalculator.java
@@ -0,0 +1,16 @@
+package com.example.Test;
+
+//FactorialCalculator.java
+public class FactorialCalculator {
+
+ // Method to calculate the factorial of a given number
+ public static int calculateFactorial(int n) {
+     if (n < 0) {
+         throw new IllegalArgumentException("Input must be a non-negative integer");
+     }
+     if (n == 0 || n == 1) {
+         return 1;
+     }
+     return n * calculateFactorial(n - 1);
+ }
+}
---
+++ src/test/java/com/example/Test/FactorialCalculatorTest.java
@@ -0,0 +1,36 @@
+package com.example.Test;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+public class FactorialCalculatorTest {
+
+	@Test
+	public void testCalculateFactorial() {
+		// Test case for calculating the factorial of 5
+		int result = FactorialCalculator.calculateFactorial(5);
+
+		// The expected result is 5! = 120
+		assertEquals(120, result);
+	}
+
+	@Test
+	public void testCalculateFactorialOfZero() {
+		// Test case for calculating the factorial of 0
+		int result = FactorialCalculator.calculateFactorial(0);
+
+		// The factorial of 0 is 1
+		assertEquals(1, result);
+	}
+
+	@Test
+	public void testCalculateFactorialOfOne() {
+		// Test case for calculating the factorial of 1
+		int result = FactorialCalculator.calculateFactorial(1);
+
+		// The factorial of 1 is 1
+		assertEquals(1, result);
+	}
+}
